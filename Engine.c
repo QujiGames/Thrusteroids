077fad2f2439387042005a597a95a9f596cedbf1
Qujam <padawanpaul@ntlworld.com>
2019-10-17 21:27:33 +0100

    initial changes
    Initial changes

---
+++ Engine.c
@@ -0,0 +1,16 @@
+#include <windows.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <assert.h>
+
+#include "Console/Console.h"
+
+
+
+void Get_Inputs()
+
+{
+
+	
+}
--- Thrusteroids.c
+++ Thrusteroids.c
@@ -1,6 +1,22 @@
-#include <iostream>
+#include "Random/Random.h"
+#include "Console/Console.h"
+#include <Windows.h>
+#include "Game.h"
+#include "Engine.h"
+
+static int bGameIsRunning = 1;
+
+
 
 int main()
 {
-	some gibberish here;
+
+	Game_Init();
+
+
+	while (bGameIsRunning)
+	{
+		Console_Printf(20, 20, "I love Potatoes!");
+	}
+
 }
