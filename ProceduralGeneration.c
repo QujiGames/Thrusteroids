6b3c31efc384a64effbd9d418b720482ea67e3a9
Robin Mukherjee <robin@otaking.co.uk>
2019-10-20 12:01:32 +0100

    added initial procedural generation

---
+++ ProceduralGeneration.c
@@ -0,0 +1,47 @@
+#include <windows.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <assert.h>
+#include <math.h>
+
+#include "Console/Console.h"
+
+static long int level_size = 10000;
+
+
+int jitter[]()
+{
+	int jitter_value;
+	
+	return jitter_value;
+}
+
+int variance()
+{
+	int variance_value;
+
+	return variance_value;
+}
+
+
+
+
+/*	
+*	x_jitter is the amount of distance away the terrain will be drawn relative to it's neighbour in the x direction
+*	y_jitter is the same for the y direction
+*	level_size is the length of the array for the level, size is determined by x distance multiplied by y distance
+*	e.g.	10000 equates to 100 x 100 level size
+*	x_variance is the randomness of the x_jitter
+*	y_variance is the randomness of the y_jitter
+*
+*
+*/
+
+
+
+void Level_Generator(int jitter(), int jitter(), int variance(), int variance(), int level_size)
+{
+
+
+}
